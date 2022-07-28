4d298957c5d2462333099f9d71d92436f111678c
andrenachtigall <an.nachtigall@inf.ufpel.edu.br>
2022-07-27 21:25:29 -0300

    primeiro commit

---
+++ Semana1/pratica/checkpoint/ex1.js
@@ -0,0 +1,13 @@
+let nome, sobrenome, diaDoNascimento, mesDoNascimento, anoDoNascimento, primeiraNota, segundaNota;
+
+nome = "André Nunes Nachtigall"
+diaDoNascimento = 01;
+mesDoNascimento = 01;
+anoDoNascimento = 1994;
+primeiraNota = 8;
+segundaNota = 8;
+
+console.log("Nome Completo: " + nome)
+console.log("Idade: " + (2022 - anoDoNascimento))
+console.log("Data de nascimento:" + diaDoNascimento + "/" + mesDoNascimento + "/" + anoDoNascimento)
+console.log("Média: " + ((primeiraNota + segundaNota) / 2))
